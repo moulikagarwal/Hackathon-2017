f484bfa2c21d6e061fa32f9b80a0b7e8fb5ad950
acolas1 <acolas1@ufl.edu>
2017-01-21 23:15:20 -0500

    tts began

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,58 @@
 var gulp = require('gulp'),
-    usemin = require('gulp-usemin'),
-    wrap = require('gulp-wrap'),
-    connect = require('gulp-connect'),
-    watch = require('gulp-watch'),
-    minifyCss = require('gulp-cssnano'),
-    minifyJs = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    less = require('gulp-less'),
-    rename = require('gulp-rename'),
-    minifyHTML = require('gulp-htmlmin');
+  usemin = require('gulp-usemin'),
+  wrap = require('gulp-wrap'),
+  connect = require('gulp-connect'),
+  watch = require('gulp-watch'),
+  minifyCss = require('gulp-cssnano'),
+  minifyJs = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  less = require('gulp-less'),
+  rename = require('gulp-rename'),
+  minifyHTML = require('gulp-htmlmin');
 
 var paths = {
-    scripts: 'src/js/**/*.*',
-    styles: 'src/less/**/*.*',
-    images: 'src/img/**/*.*',
-    templates: 'src/templates/**/*.html',
-    index: 'src/index.html',
-    bower_fonts: 'src/components/**/*.{ttf,woff,eof,svg}',
+  scripts: 'src/js/**/*.*',
+  styles: 'src/less/**/*.*',
+  images: 'src/img/**/*.*',
+  templates: 'src/templates/**/*.html',
+  index: 'src/index.html',
+  bower_fonts: 'src/components/**/*.{ttf,woff,eof,svg}',
 };
 
+
+var app = require('express')();
+var watson = require('watson-developer-cloud');
+
+var textToSpeech = watson.text_to_speech({
+  version: 'v1',
+  username: '6915b2c1-4e50-44bd-9f46-a5fcb1195fcc',
+  password: 'aB5JL1NPJ72m',
+  url: 'https://stream.watsonplatform.net/text-to-speech/api',
+});
+
+var fs = require("fs");
+app.get('/api/synthesize', function(req, res, next) {
+  var transcript = textToSpeech.synthesize(req.query);
+  transcript.on('error', function(error) {
+    next(error);
+  });
+  transcript.pipe(res);
+});
+
+app.listen(4007);
+console.log('Watson API listening at: 4007');
 /**
  * Handle bower components from index
  */
 gulp.task('usemin', function() {
-    return gulp.src(paths.index)
-        .pipe(usemin({
-            js: [minifyJs(), 'concat'],
-            css: [minifyCss({keepSpecialComments: 0}), 'concat'],
-        }))
-        .pipe(gulp.dest('dist/'));
+  return gulp.src(paths.index)
+    .pipe(usemin({
+      js: [minifyJs(), 'concat'],
+      css: [minifyCss({
+        keepSpecialComments: 0
+      }), 'concat'],
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
 /**
@@ -37,11 +61,11 @@ gulp.task('usemin', function() {
 gulp.task('build-assets', ['copy-bower_fonts']);
 
 gulp.task('copy-bower_fonts', function() {
-    return gulp.src(paths.bower_fonts)
-        .pipe(rename({
-            dirname: '/fonts'
-        }))
-        .pipe(gulp.dest('dist/lib'));
+  return gulp.src(paths.bower_fonts)
+    .pipe(rename({
+      dirname: '/fonts'
+    }))
+    .pipe(gulp.dest('dist/lib'));
 });
 
 /**
@@ -50,55 +74,55 @@ gulp.task('copy-bower_fonts', function() {
 gulp.task('build-custom', ['custom-images', 'custom-js', 'custom-less', 'custom-templates']);
 
 gulp.task('custom-images', function() {
-    return gulp.src(paths.images)
-        .pipe(gulp.dest('dist/img'));
+  return gulp.src(paths.images)
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('custom-js', function() {
-    return gulp.src(paths.scripts)
-        .pipe(minifyJs())
-        .pipe(concat('dashboard.min.js'))
-        .pipe(gulp.dest('dist/js'));
+  return gulp.src(paths.scripts)
+    .pipe(minifyJs())
+    .pipe(concat('dashboard.min.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('custom-less', function() {
-    return gulp.src(paths.styles)
-        .pipe(less())
-        .pipe(gulp.dest('dist/css'));
+  return gulp.src(paths.styles)
+    .pipe(less())
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('custom-templates', function() {
-    return gulp.src(paths.templates)
-        .pipe(minifyHTML())
-        .pipe(gulp.dest('dist/templates'));
+  return gulp.src(paths.templates)
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist/templates'));
 });
 
 /**
  * Watch custom files
  */
 gulp.task('watch', function() {
-    gulp.watch([paths.images], ['custom-images']);
-    gulp.watch([paths.styles], ['custom-less']);
-    gulp.watch([paths.scripts], ['custom-js']);
-    gulp.watch([paths.templates], ['custom-templates']);
-    gulp.watch([paths.index], ['usemin']);
+  gulp.watch([paths.images], ['custom-images']);
+  gulp.watch([paths.styles], ['custom-less']);
+  gulp.watch([paths.scripts], ['custom-js']);
+  gulp.watch([paths.templates], ['custom-templates']);
+  gulp.watch([paths.index], ['usemin']);
 });
 
 /**
  * Live reload server
  */
 gulp.task('webserver', function() {
-    connect.server({
-        root: 'dist',
-        livereload: true,
-        port: 8888
-    });
+  connect.server({
+    root: 'dist',
+    livereload: true,
+    port: 8888
+  });
 });
 
 gulp.task('livereload', function() {
-    gulp.src(['dist/**/*.*'])
-        .pipe(watch(['dist/**/*.*']))
-        .pipe(connect.reload());
+  gulp.src(['dist/**/*.*'])
+    .pipe(watch(['dist/**/*.*']))
+    .pipe(connect.reload());
 });
 
 /**
--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -2,38 +2,58 @@
  * Master Controller
  */
 
+
+
 angular.module('RDash')
-    .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
+  .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
 
-function MasterCtrl($scope, $cookieStore) {
-    /**
-     * Sidebar Toggle & Cookie Control
-     */
-    var mobileView = 992;
+function MasterCtrl($scope, $cookieStore, $http) {
+  /**
+   * Sidebar Toggle & Cookie Control
+   */
+  var mobileView = 992;
 
-    $scope.getWidth = function() {
-        return window.innerWidth;
-    };
 
-    $scope.$watch($scope.getWidth, function(newValue, oldValue) {
-        if (newValue >= mobileView) {
-            if (angular.isDefined($cookieStore.get('toggle'))) {
-                $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
-            } else {
-                $scope.toggle = true;
-            }
-        } else {
-            $scope.toggle = false;
-        }
+  $scope.getWidth = function() {
+    return window.innerWidth;
+  };
 
-    });
+  $scope.$watch($scope.getWidth, function(newValue, oldValue) {
+    if (newValue >= mobileView) {
+      if (angular.isDefined($cookieStore.get('toggle'))) {
+        $scope.toggle = !$cookieStore.get('toggle') ? false : true;
+      } else {
+        $scope.toggle = true;
+      }
+    } else {
+      $scope.toggle = false;
+    }
 
-    $scope.toggleSidebar = function() {
-        $scope.toggle = !$scope.toggle;
-        $cookieStore.put('toggle', $scope.toggle);
-    };
+  });
 
-    window.onresize = function() {
-        $scope.$apply();
-    };
+  $scope.toggleSidebar = function() {
+
+    var fs = require("fs");
+    var text = fs.readFileSync("../a.txt").toString('utf-8');
+    console.log(text);
+
+
+
+    var audio = new Audio('http://localhost:4007/api/synthesize?voice=en-US_MichaelVoice&text=');
+    audio.play();
+
+
+
+
+    $scope.toggle = !$scope.toggle;
+    $cookieStore.put('toggle', $scope.toggle);
+
+
+  };
+
+
+
+  window.onresize = function() {
+    $scope.$apply();
+  };
 }
